855a8209707cb90c58abfbb594d886a4ee9d5c2a
Md Amir Sohail <mdas442006@gmail.com>
2017-08-22 15:33:30 +0530

    A very basic Java program that just prints the String "Hello World !"

---
+++ Hello World.java
@@ -0,0 +1,12 @@
+//A very basic Java program
+//It just prints the String "Hello World !"
+
+public class Main
+{
+ 	public static void main(String s[])
+    {
+     	System.out.println("Hello World !"); // Prints a new line at the end
+      	// If you don't want to print that new line, then use the following:
+        // System.out.print("Hello World !");
+    }
+}
